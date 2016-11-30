5168a1de0a28a9abe31d2499de7f9f527d3eb7ba
Victor Caballero <vicaba@Victors-MacBook-Pro.local>
2016-11-30 23:40:28 +0100

    Development packages are now installed successfully.
    - Roles' versions updated.
    - AerisCloud.ansible-roles was not working. Remi and remi-php7 repositories are now manually installed. Epel repository is installed before installing ansible (for vagrant) since is the repo for ansible, it should be also installed via ansible playbook.
    - Be aware that the apcu.so php extension is not found although php.ini tries to load it (/etc/php.d/).

--- vagrant/shell/vagrant_main_provision.sh
+++ vagrant/shell/vagrant_main_provision.sh
@@ -22,11 +22,11 @@ fi
 echo "-> Installing Ansibe Galaxy Modules"
 
 roles_list[0]='geerlingguy.ntp,1.3.0'
-roles_list[1]='geerlingguy.apache,1.7.2'
-roles_list[2]='geerlingguy.php,3.4.0'
+roles_list[1]='geerlingguy.apache,1.7.4'
+roles_list[2]='geerlingguy.php,3.4.2'
 roles_list[3]='geerlingguy.firewall,1.0.9'
-roles_list[4]='geerlingguy.composer,1.4.1'
-roles_list[5]='geerlingguy.mysql,2.3.1'
+roles_list[4]='geerlingguy.composer,1.5.0'
+roles_list[5]='geerlingguy.mysql,2.4.2'
 roles_list[6]='AerisCloud.repos,v1.1.2'
 
 for role_and_version in "${roles_list[@]}"
